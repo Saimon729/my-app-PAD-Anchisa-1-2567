ebb10545642f95ad97a5df279d96dadeb898434d
Natthidj <nuttida162547@gmail.com>
2024-08-28 17:21:45 +0700

    firts commit

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -11,16 +11,35 @@ export async function PUT(request, { params }) {
     newImplementation: implementation,
     newEmail: email,
     newMobile: mobile,
-    newBudget:budget,
+    newBudget: budget,
     newYear: year,
     newEvaluation: evaluation,
-    newWeak:  weak,
+    newWeak: weak,
     newStrength: strength,
     newDevelopment: development,
     newSuggestion: suggestion,
-    
   } = await request.json();
   await connectMongoDB();
-  await Product.findByIdAndUpdate(id, { name, image, price, category });
+  await Product.findByIdAndUpdate(id, {
+    name,
+    project,
+    implementation,
+    email,
+    mobile,
+    budget,
+    year,
+    evaluation,
+    weak,
+    strength,
+    development,
+    suggestion,
+  });
   return NextResponse.json({ message: "Product updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const product = await Product.findOne({ _id: id });
+  return NextResponse.json({ product }, { status: 200 });
 }
