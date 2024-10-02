1939cb3e10f08e797782856bb0cce528f51ee068
Natthidj <nuttida162547@gmail.com>
2024-10-02 16:56:20 +0700

    first commit

---
+++ src/app/section1/page.tsx
@@ -0,0 +1,10 @@
+import Section1 from '@/components/sectiondata/Section1'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Section1/>
+    </div>
+  )
+}
---
+++ src/app/section6/page.tsx
@@ -0,0 +1,10 @@
+import Section6 from '@/components/sectiondata/Section6'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Section6/>
+    </div>
+  )
+}
